1f7cae1609e94ec4538ec7cc1ede4feb62cd431d
Amaari <arish2k3@gmail.com>
2023-05-17 13:43:24 +0530

    add disable_raw_mode function to disable raw mode at exit and also set original termios struct to keep track of the original state of the termios before changing values in enable_raw_mode function

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 
 struct termios orig_termios;
 
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enable_raw_mode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode);
+    struct termios raw = orig_termios;
     tcgetattr(STDIN_FILENO, &raw);
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
